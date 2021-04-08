7c968299d90bb9d64d45c6e031421a1c5cff3190
MJin-Hellobiz <jinwithgame@gmail.com>
2021-04-08 19:04:48 +0900

    refec[mission3]

---
+++ app/src/main/java/com/hellobiz/mission/mission3/signup/model/ProfileUpdateModel.kt
@@ -0,0 +1,8 @@
+package com.hellobiz.mission.mission3.signup.model
+
+import com.google.gson.annotations.SerializedName
+
+data class SignUpModel(@SerializedName("code") var code: Int,
+                       @SerializedName("message") var message: String
+)
+
